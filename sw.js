948287c36ac2289a395b1334a881507df96d0d43
Andrés Mendoza <coordinadorlogisticocdi@gmail.com>
2026-02-23 14:46:25 -0500

    deepscope

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/core/config.js');
 
 // CORREGIDO: Eliminadas notificaciones duplicadas
-const CACHE_NAME = `${CONFIG.APP_NAME}-v14.1`; // Network-First Strategy 100% Online
+const CACHE_NAME = `${CONFIG.APP_NAME}-v15.0`; // Network-First Strategy 100% Online
 
 const BASE = (new URL('.', self.location)).href;
 
